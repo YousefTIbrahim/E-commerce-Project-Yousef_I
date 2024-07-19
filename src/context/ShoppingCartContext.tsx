52302eb840a6c86bbcef538aebd2b519304ebf16
yibrahim9 <yibrahim9@gatech.edu>
2024-07-19 10:39:39 -0400

    Update ShoppingCartContext.tsx

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -14,9 +14,9 @@ type ShoppingCartContext = {
     openCart: () => void
     closeCart: () => void
     getItemQuantity: (id: number) => number
-    increaseCartQuantity: (id: number) => number
-    decreaseCartQuantity: (id: number) => number
-    removeFromCart: (id: number) => number
+    increaseCartQuantity: (id: number) => void
+    decreaseCartQuantity: (id: number) => void
+    removeFromCart: (id: number) => void
     cartQuantity: number
     cartItems: CartItem[]
 }
